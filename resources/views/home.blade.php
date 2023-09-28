7b307263fc725b44a9640682ba069584ee4b52c7
Elizabeth Silvia Pumacota Quispe <elizabethsilvia.15@gmail.com>
2023-09-27 22:31:30 -0500

    HU002 - Gestionar los usuarios y roles

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,6 @@
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">{{ __('Dashboard') }}</div>
-
                 <div class="card-body">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
@@ -14,7 +13,7 @@
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    {{ __('Hola!') }}
                 </div>
             </div>
         </div>
