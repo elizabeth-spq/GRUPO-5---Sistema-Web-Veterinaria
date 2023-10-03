1e29fa4886eb8f92991da6a5c8923ee58039c416
Elizabeth Silvia Pumacota Quispe <elizabethsilvia.15@gmail.com>
2023-10-02 19:58:12 -0500

    HU002 - Gestionar los usuarios

--- database/migrations/2013_09_25_024402_create_rol_table.php
+++ database/migrations/2013_09_25_024402_create_rol_table.php
@@ -11,25 +11,16 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('rol', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-
-            $table->string('nombre', 250);
-            $table->string('apellido', 250);
+            $table->string('nombre');
+            $table->text('modulos');
             $table->integer('estado');
-            $table->unsignedInteger('rol_id');
             $table->integer('usu_registro');
             $table->integer('usu_ult_mod');
             $table->timestamp('fec_reg')->useCurrent();
             $table->timestamp('fec_ult_mod')->useCurrent();
 
-            $table->foreign('rol_id')->references('id')
-            ->on('rol');
         });
     }
 
@@ -38,6 +29,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('rols');
+        Schema::dropIfExists('rol');
     }
 };
--- database/migrations/2023_10_02_220544_create_mascota_table.php
+++ database/migrations/2023_10_02_220544_create_mascota_table.php
@@ -11,25 +11,23 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('mascota', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-
             $table->string('nombre', 250);
-            $table->string('apellido', 250);
+            $table->string('raza', 250);
+            $table->integer('edad');
+            $table->integer('sexo');
+            $table->double('peso', 8, 2);
+            $table->string('direccion', 250);
             $table->integer('estado');
-            $table->unsignedInteger('rol_id');
+            $table->unsignedInteger('cliente_id');
             $table->integer('usu_registro');
             $table->integer('usu_ult_mod');
             $table->timestamp('fec_reg')->useCurrent();
             $table->timestamp('fec_ult_mod')->useCurrent();
 
-            $table->foreign('rol_id')->references('id')
-            ->on('rol');
+            $table->foreign('cliente_id')->references('id')
+                ->on('cliente');
         });
     }
 
@@ -38,6 +36,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('mascota');
     }
 };
