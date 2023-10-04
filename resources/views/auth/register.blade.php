056cb41de974d183a635ce00ec69c21b774a5486
Juanjose Alonso Campos Sauñi <juanjosealonsoc@gmail.com>
2023-10-03 20:54:55 -0500

    HU001-INICIAR SESION - CORRECCION DE INICIO DE SESION

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -26,12 +26,13 @@
               <a  href="http://127.0.0.1:8000" class="btn btn-dark btn-lg btn-block" type="button"> <- Ventana inicio</a>
                 <p class="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Crea una Cuenta</p>
                 
-                <form class="mx-1 mx-md-4">
+                <form action="{{route('register')}}" method="post">
+                  @csrf
 
                   <div class="d-flex flex-row align-items-center mb-4">
                     <i class="fas fa-user fa-lg me-3 fa-fw"></i>
                     <div class="form-outline flex-fill mb-0">
-                      <input type="text" id="form3Example1c" class="form-control" />
+                      <input type="text" name="name" id="form3Example1c" class="form-control" />
                       <label class="form-label" for="form3Example1c">Tu nombre</label>
                     </div>
                   </div>
@@ -39,7 +40,7 @@
                   <div class="d-flex flex-row align-items-center mb-4">
                     <i class="fas fa-envelope fa-lg me-3 fa-fw"></i>
                     <div class="form-outline flex-fill mb-0">
-                      <input type="email" id="form3Example3c" class="form-control" />
+                      <input type="email" name="email" id="form3Example3c" class="form-control" />
                       <label class="form-label" for="form3Example3c">Tu correo electronico</label>
                     </div>
                   </div>
@@ -47,7 +48,7 @@
                   <div class="d-flex flex-row align-items-center mb-4">
                     <i class="fas fa-lock fa-lg me-3 fa-fw"></i>
                     <div class="form-outline flex-fill mb-0">
-                      <input type="password" id="form3Example4c" class="form-control" />
+                      <input type="password"name="password" id="form3Example4c" class="form-control" />
                       <label class="form-label" for="form3Example4c">Tu Contraseña</label>
                     </div>
                   </div>
@@ -55,12 +56,12 @@
                   <div class="d-flex flex-row align-items-center mb-4">
                     <i class="fas fa-key fa-lg me-3 fa-fw"></i>
                     <div class="form-outline flex-fill mb-0">
-                      <input type="password" id="form3Example4cd" class="form-control" />
+                      <input type="password"name="password_confirmation" id="form3Example4cd" class="form-control" />
                       <label class="form-label" for="form3Example4cd">Confirma tu contraseña</label>
                     </div>
                   </div>
                   <div class="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
-                    <button  type="submit" class="btn btn-dark btn-lg btn-block">Registrarse</button>
+                    <button  class="btn btn-dark btn-lg btn-block" type="submit">Registrarse</button>
                   </div>
 
                 </form>
