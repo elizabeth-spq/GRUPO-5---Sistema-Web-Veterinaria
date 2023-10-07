04be8ee130de2b7b84098058a651f096371f3b03
Pablo Romario Zapata Guillermo <romahhzg@gmail.com>
2023-10-07 05:40:16 -0500

    HU002 - Gestionar los usuarios

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Str;
 
@@ -13,7 +14,7 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        /*User::create([
+        User::create([
             'name' => 'admin',
             'email' => 'admin@gmail.com',
             'email_verified_at' => now(),
@@ -22,11 +23,20 @@ class UserSeeder extends Seeder
         ])->assignRole('administrador');
 
         User::create([
-            'name' => 'juanito perez',
+            'name' => 'Juanito perez',
             'email' => 'jperez@gmail.com',
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
-        ])->assignRole('recepcionista');*/
+        ])->assignRole('veterinario');
+
+        User::create([
+            'name' => 'Maria Vega',
+            'email' => 'mvega@gmail.com',
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => Str::random(10),
+        ])->assignRole('recepcionista');
+
     }
 }
