047a3a7d6cd5e93b83fec03615a2f8997b9fa29f
Elizabeth Silvia Pumacota Quispe <elizabethsilvia.15@gmail.com>
2023-12-07 21:36:21 -0500

    HU025 - Actualizacion de Front

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,95 +2,120 @@
 <html lang="en">
 
 <head>
-  <title>Title</title>
-  <!-- Required meta tags -->
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Title</title>
+    @vite(['resources/sass/auth.scss'])
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-  <!-- Bootstrap CSS v5.2.1 -->
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
-
+    <!-- Bootstrap CSS v5.2.1 -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    <style>
+        .rounded {
+            width: auto;
+            height: 80px;
+        }
+    </style>
 </head>
 
 <body>
+    <div class="container-fluid p-0">
+        <div id="auth">
+            <div class="overlay">
+                <div class="container">
+                    <main>
+                        <div class="d-flex justify-content-center">
+                            <div class="card">
+                                <div class="card-body p-0">
+                                    <div class="row align-items-center">
+                                        <div class="col-5">
+                                            <img src="https://i.pinimg.com/474x/75/b8/59/75b8594198d0fb111afc7778cbd20fb6.jpg"
+                                                alt="login form" class="img-fluid" />
+                                        </div>
+                                        <div class="col-7">
+                                            <form action="{{ route('login') }}" method="post">
+                                                @csrf
+                                                <div class="row text-center p-4">
+                                                    <div class="col-12 d-flex justify-content-center">
+                                                        <img src="{{ URL('images/vet.PNG') }}"
+                                                            class="rounded float-start p-0">
+                                                    </div>
+                                                    <div class="col-12">
+                                                        <h1>Iniciar Sesion</h1>
+                                                    </div>
+                                                    <div class="col-12">
+                                                        <input type="email" name="email" id="form2Example17"
+                                                            class="form-control" />
+                                                        <label class="form-label" for="form2Example17">Ingresa tu
+                                                            correo electronico
+                                                            o usuario</label>
+                                                    </div>
+                                                    <div class="col-12">
+                                                        <input type="password" name="password" id="form2Example27"
+                                                            class="form-control form-control-lg" />
+                                                        <label class="form-label" for="form2Example27">Ingrese su
+                                                            contraseña</label>
+                                                    </div>
+                                                    <div class="pt-1 mb-4">
+                                                        <button class="btn btn-dark btn-lg btn-block"
+                                                            type="submit">Ingresar</button>
+                                                    </div>
 
-<section class="vh-100" style="background-color: #76e2e5;">
-  <div class="container py-5 h-100">
-    <div class="row d-flex justify-content-center align-items-center h-100">
-      <div class="col col-xl-10">
-        <div class="card" style="border-radius: 1rem;">
-          <div class="row g-0">
-            <div class="col-md-6 col-lg-5 d-none d-md-block">
-              <img src="https://i.pinimg.com/474x/75/b8/59/75b8594198d0fb111afc7778cbd20fb6.jpg"
-                alt="login form" class="img-fluid" style="border-radius: 1rem 0 0 1rem;" />
-            </div>
-            <div class="col-md-6 col-lg-7 d-flex align-items-center">
-              <div class="card-body p-4 p-lg-5 text-black">
+                                                    @if ($errors->has('isDeactivated'))
+                                                        <div class="alert alert-danger" role="alert">
+                                                            {{ $errors->first('isDeactivated') }}
+                                                        </div>
+                                                    @endif
 
-                <form action="{{route('login')}}" method="post">
-                  @csrf
+                                                    @if ($errors->has('invalidCredentials'))
+                                                        <div class="alert alert-danger" role="alert">
+                                                            {{ $errors->first('invalidCredentials') }}
+                                                        </div>
+                                                    @endif
 
-                  <div class="d-flex align-items-center mb-3 pb-1">
-                    <i class="fas fa-cubes fa-2x me-3" style="color: #ff6219;"></i>
-                    <span class="h1 fw-bold mb-0">Veterinaria "Ruiz Castro"</span>
-                  </div>
 
-                  <h5 class="fw-normal mb-3 pb-3" style="letter-spacing: 1px;">Iniciar Sesion</h5>
+                                                    <a class="small text-muted"
+                                                        href="{{ route('password.request') }}">Olvidate tu
+                                                        contraseña?</a>
+                                                    <p class="mb-5 pb-lg-2" style="color: #393f81;">Desea crear una
+                                                        cuenta? <a href="{{ route('register') }}"
+                                                            style="color: #393f81;">Registrarse</a>
+                                                    </p>
 
-                  <div class="form-outline mb-4">
-                    <input type="email" name="email" id="form2Example17" class="form-control form-control-lg" />
-                    <label class="form-label" for="form2Example17">Ingresa tu correo electronico o usuario</label>
-                  </div>
+                                                    <a href="http://127.0.0.1:8000"
+                                                        class="btn btn-dark btn-lg btn-block" type="button">Ventana
+                                                        inicio -></a>
+                                                    <!--<a href="#!" class="small text-muted">Privacy policy</a>-->
 
-                  <div class="form-outline mb-4">
-                    <input type="password"  name="password" id="form2Example27" class="form-control form-control-lg" />
-                    <label class="form-label" for="form2Example27">Ingrese su contraseña</label>
-                  </div>
+                                                </div>
 
-                  <div class="pt-1 mb-4">
-                    <button class="btn btn-dark btn-lg btn-block" type="submit">Ingresar</button>
-                  </div>
 
-                    @if ($errors->has('isDeactivated'))
-                        <div class="alert alert-danger" role="alert">
-                            {{ $errors->first('isDeactivated') }}
+                                            </form>
+                                        </div>
+                                    </div>
+
+                                </div>
+                            </div>
                         </div>
-                    @endif
-
-                    @if ($errors->has('invalidCredentials'))
-                        <div class="alert alert-danger" role="alert">
-                            {{ $errors->first('invalidCredentials') }}
-                        </div>
-                    @endif
-
-
-                  <a class="small text-muted" href="{{ route('password.request') }}">Olvidate tu contraseña?</a>
-                  <p class="mb-5 pb-lg-2" style="color: #393f81;">Desea crear una cuenta? <a href="{{ route('register') }}"
-                      style="color: #393f81;">Registrarse</a></p>
-
-                  <a  href="http://127.0.0.1:8000" class="btn btn-dark btn-lg btn-block" type="button">Ventana inicio -></a>
-                  <!--<a href="#!" class="small text-muted">Privacy policy</a>-->
-                </form>
-              </div>
+                    </main>
+                </div>
             </div>
-          </div>
         </div>
-      </div>
     </div>
-  </div>
-</section>
+
+
 
     <!-- place footer here -->
-  </footer>
-  <!-- Bootstrap JavaScript Libraries -->
-  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
-    integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous">
-  </script>
+    </footer>
+    <!-- Bootstrap JavaScript Libraries -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
+        integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous">
+    </script>
 
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js"
-    integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous">
-  </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js"
+        integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous">
+    </script>
 </body>
 
 </html>
